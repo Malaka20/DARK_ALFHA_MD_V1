24c47c511b65300a8b7d58c0c91771ae168d8ecb
Malaka-KG <darkalfhabot@gmail.com>
2024-10-24 22:44:14 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,7 +10,20 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+
+let des = `*👋 Hello ${pushname} I'm alive now*
+*😉🇱🇰 I'm 𝗜 𝘿𝘼𝙍𝙆-𝘼𝙇𝙁𝙃𝘼-𝗠𝗗 Whatsapp Bot* ✓
+
+> *Version:* 8.0.0
+> *Ram usage:* 43.46MB / 15981MB
+> *Runtime:* 3 hours, 7 minutes, 35 seconds
+> *HostName:* fv-az984-882
+
+*🪀 𝙈𝘼𝙇𝘼𝙆𝘼 𝘽𝙔 𝘿𝘼𝙍𝙆-𝘼𝙇𝙁𝙃𝘼-𝙈𝘿 🪀*
+
+`
+
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
