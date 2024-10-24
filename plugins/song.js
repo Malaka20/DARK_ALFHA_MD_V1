53f3e1491ca26346559cd89df5bd9df9ddfb2974
Malaka-KG <darkalfhabot@gmail.com>
2024-10-24 20:01:06 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -1,74 +1,48 @@
 const {cmd , commands} = require('../command')
-const yts = require('yt-search');
-const fg = require('api-dylux');
+const fg = require('api-dylux')
+const yts = require('yt-search')
 
-// ---------------------- Song Download -----------------------
+
 cmd({
-    pattern: 'song',
-    desc: 'download songs',
+    pattern: "song",
+    desc: "download songs",
+    category: "download songs",
     react: "🎧",
-    category: 'download',
     filename: __filename
 },
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        if (!q) return reply('*Please enter a query or a url !*');
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me url or title")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
 
-        const search = await yts(q);
-        const data = search.videos[0];
-        const url = data.url;
+let desc = `
+⭐ *DARK_ALFHA_MD SONG DOWNLOADER* ⭐
 
-        let desc = `🎶 *_QUEEN-CHOOTY-NELUMI-MD AUDIO DOWNLOADER_* 🎶
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
 
-┌───────────────────
-├ ℹ️ *Title:* ${deta.title}
-├ 👤 *Author:* ${deta.author.name}
-├ 👁️‍🗨️ *Views:* ${deta.views}
-├ 🕘 *Duration:* ${deta.timestamp}
-├ 📌 *Ago:* ${deta.ago}
-└───────────────────
+MADE BY DARK_ALFHA_MD ✅
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
 
-💻 https://github.com/Navinofc44/QUEEN-CHOOTY-NELUMI-MD-V2
+//download audio
 
-*Choose Your Download Format*
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
 
-*1 Audio File🎶*
-*2 Document File📁*
+//send audio + document message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE BY MALAKA"},{quoted:mek})
 
-> *QUEEN-CHOOTY-CHOOTY-MD*`;
 
-        const vv = await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
-
-        conn.ev.on('messages.upsert', async (msgUpdate) => {
-            const msg = msgUpdate.messages[0];
-            if (!msg.message || !msg.message.extendedTextMessage) return;
-
-            const selectedOption = msg.message.extendedTextMessage.text.trim();
-
-            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
-                switch (selectedOption) {
-                    case '1':
-                        let down = await fg.yta(url);
-                        let downloadUrl = down.dl_url;
-                        await conn.sendMessage(from, { audio: { url:downloadUrl }, caption: '*ᴘᴀᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴍɪᴋᴀ ᴍᴀɪɴ*', mimetype: 'audio/mpeg'},{ quoted: mek });
-                        break;
-                    case '2':               
-                        // Send Document File
-                        let downdoc = await fg.yta(url);
-                        let downloaddocUrl = downdoc.dl_url;
-                        await conn.sendMessage(from, { document: { url:downloaddocUrl }, caption: '*ᴘᴀᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴍɪᴋᴀ ᴍᴀɪɴ*', mimetype: 'audio/mpeg', fileName:data.title + ".mp3"}, { quoted: mek });
-                        await conn.sendMessage(from, { react: { text: '✅', key: mek.key } })
-                        break;
-                    default:
-                        reply("Invalid option. Please select a valid option🔴");
-                }
-
-            }
-        });
-
-    } catch (e) {
-        console.error(e);
-        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
-        reply('An error occurred while processing your request.');
-    }
-});
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
