4859915281455bc5597ee7b8a0d5a08b62aa4392
Malaka-KG <darkalfhabot@gmail.com>
2024-10-24 22:45:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/GWyFJPj/20241013-212512.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am Dark alfha i am alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 };
