bf17c16090d88c332952319782a91e2bfe465491
Malaka-KG <darkalfhabot@gmail.com>
2024-10-25 22:07:58 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -6,13 +6,11 @@ const yts = require('yt-search')
 cmd({
     pattern: "song",
     desc: "download songs",
-    category: "download songs",
+    category: "download",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const search = await yts(q)
-const data = search.videos[0];
 const url = data.url
 
 //download audio
