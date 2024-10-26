8b21c35edee915c897c59927abf9211cf3693fac
Malaka-KG <darkalfhabot@gmail.com>
2024-10-26 22:23:05 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -1,9 +1,6 @@
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux');
 const yts = require('yt-search');
-const googleTTS = require("google-tts-api");
-const ytdl = require('ytdl-secktor');
-const fs = require('fs-extra');
 
 
 cmd({
@@ -95,43 +92,3 @@ console.log(e)
 reply(`${e}`)
 }
 })
-
-
-
-//---------------------------------------------------------------------------
-     cmd({
-        pattern: "yts",
-        desc: "Gives descriptive info of query from youtube..",
-        category: "downloader",
-        filename: __filename,
-        use: '<yt search text>',
-    },
-    async(Void, citel, text) => {
-        let yts = require("secktor-pack");
-        if (!text) return citel.reply(`Example : ${prefix}yts ${tlang().title} WhatsApp Bot`);
-        let search = await yts(text);
-        let textt = "*YouTube Search*\n\n Result From " + text + "\n\n───────────────────\n";
-        let no = 1;
-        for (let i of search.all) {
-            textt += `⚡ No : ${no++}\n ❤Title : ${i.title}\n♫ Type : ${
-      i.type
-    }\n🙈Views : ${i.views}\n⌛Duration : ${
-      i.timestamp
-    }\n🌟Upload At : ${i.ago}\n👑Author : ${i.author.name}\n🎵Url : ${
-      i.url
-    }\n\n──────────────\n\n`;
-        }
-        return Void.sendMessage(citel.chat, {
-            image: {
-                url: search.all[0].thumbnail,
-            },
-            caption: textt,
-        }, {
-            quoted: citel,
-        });
-    }
-)
-
-  
-
-
