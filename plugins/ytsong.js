11d850acb6e65edb0952d63a4c14100291f3205f
Malaka-KG <darkalfhabot@gmail.com>
2024-10-26 23:53:24 +0530

    Update ytsong.js

--- plugins/ytsong.js
+++ plugins/ytsong.js
@@ -6,10 +6,10 @@ const fg = require('api-dylux')
 const { fetchJson } = require("../lib/functions")
 
 cmd({
-    pattern: "video",
-    desc: "downlode videos",
+    pattern: "song",
+    desc: "downlode songs",
     category: "downlode",
-    react: "🎬",
+    react: "🎧",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
