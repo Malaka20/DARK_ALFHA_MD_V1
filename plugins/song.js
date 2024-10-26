872222e2b3acbe8df9354e90704312fd8595e2ae
Malaka-KG <darkalfhabot@gmail.com>
2024-10-26 18:15:09 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -1,5 +1,6 @@
 const {cmd , commands} = require('../command')
-const yts = require('yt-search')
+const fg = require('api-dylux');
+const yts = require('yt-search');
 
 
 cmd({
@@ -89,3 +90,7 @@ console.log(e)
 reply(`${e}`)
 }
 })
+
+  
+
+
