a5c1ae07dadb04ed1f489e0cef97fe84e774b055
Malaka-KG <darkalfhabot@gmail.com>
2024-10-27 01:14:59 +0530

    Update ytvideo.js

--- plugins/ytvideo.js
+++ plugins/ytvideo.js
@@ -1,6 +1,7 @@
 //Malaka  by DARKALFHAMD 🙋
 
 const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
 const yts = require('yt-search')
 const { fetchJson } = require("../lib/functions")
 
@@ -49,8 +50,53 @@ mimetype: "video/mp4",
  fileName: `${data.result.title}.mp4`,caption: `*© ᴍᴀʟᴀᴋᴀ-ᴍᴅ ʙʏ ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ · · ·* 👩‍💻`}, { quoted: mek })
 
 }catch(e){
-    console.log(e)
-    reply(`${e}`)
+console.log(e)
+reply(`${e}`)
 }
+})
+
+cmd({
+    pattern: "song",
+    desc: "downlode songs",
+    category: "downlode",
+    react: "🎧",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me url or title")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+*──────────────────*
+_*🌟 SONG DＯＷＮＬＯＤＥＲ 🌟*_
+*──────────────────*
+
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+MADE BY 𝘿𝘼𝙍𝙆-𝘼𝙇𝙁𝙃𝘼-𝙈𝘿 ✅
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio + document message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE BY MALAKA"},{quoted:mek})
+
+
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
 }
-)
+})
