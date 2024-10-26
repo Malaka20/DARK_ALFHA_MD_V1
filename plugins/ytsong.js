bceb98cea40d0044a346c1fc99add71deb649f42
Malaka-KG <darkalfhabot@gmail.com>
2024-10-27 00:55:48 +0530

    Update ytsong.js

--- plugins/ytsong.js
+++ plugins/ytsong.js
@@ -1,9 +1,8 @@
-//Malaka  by DARKALFHAMD 🙋
-
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
 const { fetchJson } = require("../lib/functions")
+
 
 cmd({
     pattern: "song",
@@ -14,46 +13,37 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("*Please give me a title*")
-let search = await yts(q)
-let link = search.all[0].url
+if(!q) return reply("Please give me url or title")
+const search = await yts(q)
+const data = search.videos[0].url
 const url = data.url
+
 let desc = `
-*──────────────────*
-_*🌟 VIDEO DＯＷＮＬＯＤＥＲ 🌟*_
-*──────────────────*
+⭐ *𝘿𝘼𝙍𝙆-𝘼𝙇𝙁𝙃𝘼-𝙈𝘿 SONG DOWNLOADER* ⭐
 
- *Title :* ${search.all[0].title}
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
 
- *Description :* ${search.all[0].description}
-
- *Duration :* ${search.all[0].timestamp}
-
- *Ago :* ${search.all[0].ago}
-
- *Views :* ${search.all[0].views}
-
- *URL :* ${search.all[0].url}
-
- _ᴍᴀʟᴀᴋᴀ-ᴍᴅ ʙʏ ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ ꜰɪʟʟ_
-
-> MADE BY 𝘿𝘼𝙍𝙆-𝘼𝙇𝙁𝙃𝘼-𝙈𝘿
+MADE BY 𝘿𝘼𝙍𝙆-𝘼𝙇𝙁𝙃𝘼-𝙈𝘿 ✅
 `
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
 
-await conn.sendMessage(from,{image:{url: search.all[0].thumbnail},caption:desc},{quoted:mek})
-
-
-        let data = await fetchJson (`https://api.dreaded.site/api/ytdl/song?url=${link}`)
+//download audio
 
 let down = await fg.yta(url)
 let downloadUrl = down.dl_url
-    
+
+//send audio + document message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
 await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE BY MALAKA"},{quoted:mek})
-    
+
+
+  
 }catch(e){
-    console.log(e)
-    reply(`${e}`)
+console.log(e)
+reply(`${e}`)
 }
-}
-)
+})
