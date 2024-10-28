367190bac95659ce9f038f476b87ebd38233d1af
Malaka-KG <darkalfhabot@gmail.com>
2024-10-28 21:29:04 +0530

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -49,4 +49,32 @@ reply('This url type is not working !!')
 }
 })
 
+cmd({
+    pattern: "dapk",
+    react: "📥",
+    dontAddCommandList: true,
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
+if(!q) return await conn.sendMessage(from , { text: 'Need apk link...' }, { quoted: mek } ) 
+const data = await apkdl.download(q)
+let listdata = `📚 Name : ${data.name}
+📦 Developer : ${data.package}
+⬆️ Last update : ${data.lastup}
+📥 Size : ${data.size}`
+await conn.sendMessage(from, { image: { url: data.icon }, caption: listdata }, { quoted: mek })
+//if (data.size.includes('GB')) return await conn.sendMessage(from , { text: 'File size is too big...' }, { quoted: mek } )
+//if (data.size.includes('MB') && data.size.replace(' MB','') > config.MAX_SIZE) return await conn.sendMessage(from , { text: 'File size is too big...' }, { quoted: mek } )
+let sendapk = await conn.sendMessage(from , { document : { url : data.dllink } , mimetype : 'application/vnd.android.package-archive' , fileName : data.name + '.' + 'apk',caption: '' } , { quoted: mek })
+await conn.sendMessage(from, { react: { text: '📁', key: sendapk.key }})
+await conn.sendMessage(from, { react: { text: '✔', key: mek.key }})
+} catch (e) {
+    reply('ERROR !!')
+    console.log(e)
+}
+})
+
+
 // https://whatsapp.com/channel/0029VaaPfFK7Noa8nI8zGg27
