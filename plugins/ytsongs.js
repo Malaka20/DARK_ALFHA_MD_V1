5e0d256e130bb08543786ea7a8af1c980d7044f5
Malaka-KG <darkalfhabot@gmail.com>
2024-10-28 13:44:26 +0530

    Update ytsongs.js

--- plugins/ytsongs.js
+++ plugins/ytsongs.js
@@ -20,64 +20,14 @@ const {
 var sizetoo =  "_This file size is too big_"
 const yts = require("ytsearch-venom")
 const fg = require('api-dylux')
-const yts = require('yt-search')
+
 
 
 let wm = config.FOOTER
 let newsize = config.MAX_SIZE * 1024 * 1024
 
-cmd({
-    pattern: "songs",
-    alias: ["ytmp3","play"],
-    use: '.song lelena',
-    react: "🎧",
-    desc: 'Download audios from youtube',
-    category: "download",
-    filename: __filename
 
-},
 
-    async (conn, m, mek, { from, q, reply }) => {
-        try {
-            if (!q) return await reply('*Please enter a query or a url!*')
-            const url = q.replace(/\?si=[^&]*/, '');
-            var results = await yts(url);
-            var result = results.videos[0]
-         let caption = ` 🪔 *Y T - S O N G*\n\n`
-         caption += `	•  *Title* : ${result.title}\n`
-         caption += `	•  *Views* : ${result.views}\n`
-         caption += `	•  *Duration* : ${result.duration}\n`
-         caption += `	◦  *URL* : ${result.url}\n\n`
-
-            let buttons = [
-            {
-                name: "quick_reply",
-                buttonParamsJson: JSON.stringify({
-                    display_text: "Audio",
-                    id: `.ytaa ${result.url}`
-                }),
-            },
-            {
-                name: "quick_reply",
-                buttonParamsJson: JSON.stringify({
-                    display_text: "Document",
-                    id: `.ytad ${result.url}±${result.title}`
-                }),
-            }
-            ]
-            let message = {
-                image: result.thumbnail,
-                header: '',
-                footer: wm,
-                body: caption
-
-            }
-            return await conn.sendButtonMessage(from, buttons, m, message)
-        } catch (e) {
-            console.log(e)
-            reply('*Error !!*')
-        }
-    })
 
 cmd({
     pattern: "mp3",
