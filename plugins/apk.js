f5a0324e8b8c934d3e8c75016ba039987b9e7be9
Malaka-KG <darkalfhabot@gmail.com>
2024-10-28 21:27:51 +0530

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -77,29 +77,3 @@ return await conn.sendButtonMessage(from, buttons, m, opts)
    console.log(e)
 }
 })
-cmd({
-    pattern: "dapk",
-    react: "📱",
-    dontAddCommandList: true,
-    filename: __filename
-},
-async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
-if(!q) return await conn.sendMessage(from , { text: 'Need apk link...' }, { quoted: mek } ) 
-const data = await apkdl.download(q)
-let listdata = `📚 Name : ${data.name}
-📦 Developer : ${data.package}
-⬆️ Last update : ${data.lastup}
-📥 Size : ${data.size}`
-await conn.sendMessage(from, { image: { url: data.icon }, caption: listdata }, { quoted: mek })
-//if (data.size.includes('GB')) return await conn.sendMessage(from , { text: 'File size is too big...' }, { quoted: mek } )
-//if (data.size.includes('MB') && data.size.replace(' MB','') > config.MAX_SIZE) return await conn.sendMessage(from , { text: 'File size is too big...' }, { quoted: mek } )
-let sendapk = await conn.sendMessage(from , { document : { url : data.dllink } , mimetype : 'application/vnd.android.package-archive' , fileName : data.name + '.' + 'apk',caption: '' } , { quoted: mek })
-await conn.sendMessage(from, { react: { text: '📁', key: sendapk.key }})
-await conn.sendMessage(from, { react: { text: '✔', key: mek.key }})
-} catch (e) {
-    reply('ERROR !!')
-    console.log(e)
-}
-})
