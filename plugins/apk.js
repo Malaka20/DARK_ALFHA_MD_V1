eb24b1b1befb6a2b47625051cede0fcff75acfdd
Malaka-KG <darkalfhabot@gmail.com>
2024-10-29 01:27:21 +0530

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -5,26 +5,6 @@ const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, 
 const apkdl = require('../lib/apkdl')
 
 cmd({
-    pattern: "apk",
-    alias: ["modapk"],
-    desc: "download apks",
-    category: "download",
-    react: "🔎",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        if (!q && !q.startsWith("https://")) return reply("❗Apk Not Found,Sorry")
-        //fetch data from api  
-        let data = await fetchJson(`${baseUrl}/api/apkdl?url=${q}`)
-        reply("*Downloading...*")
-        await conn.sendMessage(from, { document: { url: data.data.link_1 }, fileName: data.data.name, mimetype: data.data.file_type, caption: cap }, { quoted: mek })                                                                                                                 
-    } catch (e) {
-        console.log(e)
-        reply(`${e}`)
-    }
-})
-cmd({
     pattern: "dapk",
     react: "📥",
     dontAddCommandList: true,
