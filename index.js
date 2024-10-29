cfb592a0717f4d7dc4530b228a22c1a618c41217
Malaka-KG <darkalfhabot@gmail.com>
2024-10-30 01:17:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-
+const prefix = '.'
 
 const ownerNumber = ['94704243771']
 
@@ -40,15 +40,6 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
-//===========connect mongodb===================
-const connectDB = require('./lib/mongodb')
-connectDB();
-//==============================================
-const {readEnv} = require('./lib/database')   
-const config = await readEnv();
-const prefix = config.PREFIX
-//==============================================
-
 console.log("Connecting Darkalfha bot 😻...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
