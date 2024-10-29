93484e7b7d898e444f2dcb183ae8dd6efe735b8e
Malaka-KG <darkalfhabot@gmail.com>
2024-10-29 20:51:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,4 +10,6 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/XWggNqz/20241027-181936.jp
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
 };
