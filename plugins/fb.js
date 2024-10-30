1a8965669d67635518611004c871617613cde6c8
Malaka-KG <darkalfhabot@gmail.com>
2024-10-30 19:22:09 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -5,12 +5,12 @@ const { cmd, commands } = require('../command')
 // FETCH API URL
 let baseUrl;
 (async () => {
-    let baseUrlGet = await fetchJson(`https://dark-yasiya-api-new.vercel.app`)
+    let baseUrlGet = await fetchJson(`https://raw.githubusercontent.com/prabathLK/PUBLIC-URL-HOST-DB/main/public/url.json`)
     baseUrl = baseUrlGet.api
 })();
 
 
-const yourName = "©ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ ʙʏ ᴍᴀʟᴀᴋᴀ-ᴍᴅ♫🪀";
+const yourName = "*©ᴍᴀʟᴀᴋᴀ-ᴍᴅ ʙʏ ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛッ*";
 
 
 
@@ -20,12 +20,11 @@ cmd({
     alias: ["facebook"],
     desc: "download fb videos",
     category: "download",
-    react: "📥",
+    react: "🔎",
     filename: __filename
 },
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-                  
         if (!q && !q.startsWith("https://")) return reply("give me fb url")
         //fetch data from api  
         let data = await fetchJson(`${baseUrl}/api/fdown?url=${q}`)
@@ -46,7 +45,7 @@ cmd({
     alias: ["googledrive"],
     desc: "download gdrive files",
     category: "download",
-    react: "📥",
+    react: "🔎",
     filename: __filename
 },
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
@@ -61,3 +60,4 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
         reply(`${e}`)
     }
 })
+
