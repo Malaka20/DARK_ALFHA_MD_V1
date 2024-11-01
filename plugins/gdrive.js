97f00aeb0f06ed49db43b47c1883cbc63befef37
Malaka-KG <darkalfhabot@gmail.com>
2024-11-01 22:44:26 +0530

    Update and rename fb.js to gdrive.js

--- plugins/gdrive.js
+++ plugins/gdrive.js
@@ -2,35 +2,6 @@ const { fetchJson } = require('../lib/functions')
 const config = require('../config')
 const { cmd, commands } = require('../command')
 
-const yourName = "*©ᴍᴀʟᴀᴋᴀ-ᴍᴅ ʙʏ ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛッ*";
-
-
-//fb downloader
-cmd({
-    pattern: "fb",
-    alias: ["facebook"],
-    desc: "download fb videos",
-    category: "download",
-    react: "🔎",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        if (!q && !q.startsWith("https://")) return reply("give me fb url")
-        //fetch data from api  
-        let data = await fetchJson(`${baseUrl}/api/fdown?url=${q}`)
-        reply("*Downloading...*")
-        //send video (hd,sd)
-        await conn.sendMessage(from, { video: { url: data.data.hd }, mimetype: "video/mp4", caption: `- HD\n\n ${yourName}` }, { quoted: mek })
-        await conn.sendMessage(from, { video: { url: data.data.sd }, mimetype: "video/mp4", caption: `- SD \n\n ${yourName}` }, { quoted: mek })  
-    } catch (e) {
-        console.log(e)
-        reply(`${e}`)
-    }
-})
-
-
-//gdrive(google drive) dl
 cmd({
     pattern: "gdrive",
     alias: ["googledrive"],
