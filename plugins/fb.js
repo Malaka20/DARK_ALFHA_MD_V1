4a74c960234c32276e1895f0e8f48fe0ff6cf46a
Malaka-KG <darkalfhabot@gmail.com>
2024-11-01 19:50:04 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -2,69 +2,61 @@ const { fetchJson } = require('../lib/functions')
 const config = require('../config')
 const { cmd, commands } = require('../command')
 
-const metadata = [
-    {
-        "command": "fb",
-        "desc": "Download facebook videos",
-        "example": "fb https://web.facebook.com/watch/?mibextid=6aamW6&v=535935245442639",
-        "type": "download"
+// FETCH API URL
+let baseUrl;
+(async () => {
+    let baseUrlGet = await fetchJson(`https://dark-yasiya-api-new.vercel.app/news`)
+    baseUrl = baseUrlGet.api
+})();
+
+
+const yourName = "*©ᴍᴀʟᴀᴋᴀ-ᴍᴅ ʙʏ ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛッ*";
+
+
+
+//fb downloader
+cmd({
+    pattern: "fb",
+    alias: ["facebook"],
+    desc: "download fb videos",
+    category: "download",
+    react: "🔎",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q && !q.startsWith("https://")) return reply("give me fb url")
+        //fetch data from api  
+        let data = await fetchJson(`${baseUrl}/api/fdown?url=${q}`)
+        reply("*Downloading...*")
+        //send video (hd,sd)
+        await conn.sendMessage(from, { video: { url: data.data.hd }, mimetype: "video/mp4", caption: `- HD\n\n ${yourName}` }, { quoted: mek })
+        await conn.sendMessage(from, { video: { url: data.data.sd }, mimetype: "video/mp4", caption: `- SD \n\n ${yourName}` }, { quoted: mek })  
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
     }
-];
-
-const system = require('../src/system');
-const config = require('../config');
-const { fetchFbVideoDetails } = require('../lib/fb-core');
-
-module.exports = async (sock, m, query, callback) => {
-
-              
-    const data = await fetchFbVideoDetails(query);   
-    await sock.sendMessage(m.key.remoteJid, { react: { text: "🔎", key: m.key } });
-               
-
-               
-    if (data) { 
-
-        await sock.sendMessage(m.key.remoteJid, { react: { text: "🗃️", key: m.key } });
-        
-
-                    const lite = '`';
-
-                    const BodyMessage =  
-`╭─━━━━━━( *● WT-MD ●* )━━━━━─◉
-│
-│  ${lite}FB VIDEO DOWNLOADER${lite}
-│
-╰─━━━━━━━━━━━━━━━━━━━─◉`;
-       
-                        const FooterMessage = system.botFooter;
-                        const Buttons = [
-                            {
-                                "text": "HD (720p)",
-                                "type": "reply", 
-                                "id": `${config.Prefix}fbhd ${query}-btn`
-                            },
-                            {
-                                "text": "SD (360p)",
-                                "type": "reply", 
-                                "id": `${config.Prefix}fbsd ${query}-btn`
-                            },
-                        ];
-
-                        await callback({
-                            BodyMessage,
-                            FooterMessage,
-                            Buttons,
-                            MType: 'buttonMessage',
-                        });
-                } else {
-                    BodyMessage = '*Video not found (404)*';    
-                    callback({
-                        BodyMessage
-                    });
-                    await sock.sendMessage(m.key.remoteJid, { react: { text: "⚠️", key: m.key } });
-        
-                }
+})
 
 
-};
+//gdrive(google drive) dl
+cmd({
+    pattern: "gdrive",
+    alias: ["googledrive"],
+    desc: "download gdrive files",
+    category: "download",
+    react: "🔎",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q && !q.startsWith("https://")) return reply("give me gdrive url")
+        //fetch data from api  
+        let data = await fetchJson(`${baseUrl}/api/gdrivedl?url=${q}`)
+        reply("*Downloading...*")
+        await conn.sendMessage(from, { document: { url: data.data.download }, fileName: data.data.fileName, mimetype: data.data.mimeType, caption: `${data.data.fileName}\n\n${yourName}` }, { quoted: mek })                                                                                                                 
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
