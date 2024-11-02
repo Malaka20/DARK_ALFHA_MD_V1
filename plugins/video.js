a82b48b89171235be49bbdefcb3c43322aa65cd5
Malaka-KG <darkalfhabot@gmail.com>
2024-11-02 23:52:21 +0530

    Create video.js

---
+++ plugins/video.js
@@ -0,0 +1,54 @@
+//Created by Sadeesha Coder 🙋
+
+const {cmd , commands} = require('../command')
+const yts = require('yt-search')
+const { fetchJson } = require("../lib/functions")
+
+cmd({
+    pattern: "video",
+    desc: "downlode videos",
+    category: "downlode",
+    react: "🎬",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*Please give me a title*")
+let search = await yts(q)
+let link = search.all[0].url
+let desc = `
+*──────────────────*
+_*🌟 VIDEO DＯＷＮＬＯＤＥＲ 🌟*_
+*──────────────────*
+
+ *Title :* ${search.all[0].title}
+
+ *Description :* ${search.all[0].description}
+
+ *Duration :* ${search.all[0].timestamp}
+
+ *Ago :* ${search.all[0].ago}
+
+ *Views :* ${search.all[0].views}
+
+ *URL :* ${search.all[0].url}
+
+> 𝙳𝙰𝚁𝙺-𝙰𝙻𝙵𝙷𝙰-𝙱𝙾𝚃 👩‍💻
+`
+
+await conn.sendMessage(from,{image:{url: search.all[0].thumbnail},caption:desc},{quoted:mek})
+
+
+        let data = await fetchJson (`https://api.dreaded.site/api/ytdl/video?url=${link}`)
+
+await conn.sendMessage(from, {
+  video: {url: data.result.downloadLink},
+mimetype: "video/mp4",
+ fileName: `${data.result.title}.mp4`,caption: `*© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴇᴇꜱʜᴀ ᴄᴏᴅᴇʀ · · ·* 🧑🏻‍💻`}, { quoted: mek })
+
+}catch(e){
+    console.log(e)
+    reply(`${e}`)
+}
+}
+)
