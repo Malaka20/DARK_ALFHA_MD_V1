f74f77ae2041618503117e627e330d3bd19f4200
Malaka-KG <darkalfhabot@gmail.com>
2024-11-04 17:18:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "enter mongodb here",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/XWggNqz/20241027-181936.jpg",
+BOT_NUMBER: process.env.BOT_NUMBER || "94704243771",
 };
