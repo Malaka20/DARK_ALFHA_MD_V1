e8159f2fd65cd21ea35666e32ce2409a58f428f2
Malaka-KG <darkalfhabot@gmail.com>
2024-11-08 12:54:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "enter mongodb here",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/XWggNqz/20241027-181936.jpg",
 BOT_NUMBER: process.env.BOT_NUMBER || "94704243771",
+LANG: process.env.BOT_LANG || 'EN' ,
 };
