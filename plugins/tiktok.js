fcf2331ebdd54fa4fa4d8b0045f3f992a86e76db
Malaka-KG <darkalfhabot@gmail.com>
2024-11-09 14:43:50 +0530

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,62 +1,111 @@
-// TIKTOK DOWNLOAD COMMAND
+const config = require('../config')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const { Tiktok } = require('../lib/tiktok')
+function regtik(url) {
+return url.includes('tiktok.com')
+}
 
-const { cmd } = require('../command')
-const { fetchJson } = require('../lib/functions')
+var desc =''
+if(config.LANG === 'SI') desc = "Tiktok වෙතින් වීඩියෝ බාගත කරයි."
+else desc = "Download videos from Facebook."
 
-const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
+var N_FOUND =''
+if(config.LANG === 'SI') N_FOUND = "*මට කිසිවක් සොයාගත නොහැකි විය :(*"
+else N_FOUND = "*I couldn't find anything :(*"
+
+var urlneed =''
+if(config.LANG === 'SI') urlneed = "*කරුණාකර Tiktok video url එකක් ලබා දෙන්න*"
+else urlneed = "*Please give me tiktok video url..*"
 
 
 cmd({
     pattern: "tiktok",
-    alias: ["tt","ttdown"],
-    react: "⬇️",
-    desc: "",
+    alias: ["ttdl"],
+    react: '🏷️',
+    desc: desc,
     category: "download",
-    use: '.tiktok < url >',
+    use: '.tiktok <Tiktok link>',
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, reply, q }) => {
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-  
-if(!q) return await reply("Please give me tiktok url");
-  if(!q.includes('tiktok.com')) return await reply("This url is invalid");
-  
-const tiktok = await fetchJson(`${apilink}/download/tiktok?url=${q}`);
-  
-const msg = `
-*──────────────────*
-_*🪀 TIKTOK DＯＷＮＬＯＤＥＲ 🪀*_
-*──────────────────*
+if (!regtik(q)) return await  reply(urlneed)
+var l = ''
+let tiktok = await fetchJson('https://api.sdbots.tech/tiktok?url=' + q)
+if(tiktok.msg == 'OK') {
+let data = tiktok
+l = {
+title: data.result.desc,
+nowm: data.result.withoutWaterMarkVideo,
+watermark: data.result.waterMarkVideo,
+audio: data.result.music,
+thumbnail: data.result.cover,
+author: data.result.author
+}
+}
+else {
+let data = await Tiktok(q)
+l = data
+}
 
 
-• 🪀*Title* - ${tiktok.result.title}
+let dat = `[🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚]
 
-• 🪀*Author* - ${tiktok.result.author}
+*TIKTOK DOWNLOADER*
 
-• 🪀*Duration* - ${tiktok.result.duration}
+*📃 Title:* ${l.title}
+*✍🏼 Author:* ${l.author}`
 
-• 🪀*Views* - ${tiktok.result.views}  
+let sections = [
+       {
+	title: "",
+	rows: [
+    {title: "1", rowId: `${prefix}dvideo ${l.nowm}`, description: 'video without Watermark'},
+    {title: "2", rowId: `${prefix}dvideo ${l.watermark}`, description: 'Video with Watermark'},
+    {title: "3", rowId: `${prefix}dau ${l.audio}`, description: 'Download audio'},
+	]
+    } 
+]
+const listMessage = {
+  image: { url: l.thumbnail },
+  caption: dat,
+  footer: config.FOOTER,
+  buttonText: "🔢 Reply below number,",
+  sections,
+contextInfo: {
+				
+				externalAdReply: { 
+					title: '🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚',
+					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
+					mediaType: 1,
+					sourceUrl: "" ,
+          thumbnailUrl: 'https://telegra.ph/file/ba8ea739e63bf28c30b37.jpg' ,
+					renderLargerThumbnail: false,
+          showAdAttribution: true
+         }}	
+}
 
-• 🪀*ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ
-`
-  
-// SEND DETAILS
-await conn.sendMessage( from, { image: { url: tiktok.result.cover || '' }, caption: msg }, { quoted: mek });
+await conn.replyList(from, listMessage ,{ quoted : mek }) 
 
-// SEND WATER MARK VIDEO
-await conn.sendMessage(from, { video: { url: tiktok.result.wmVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nWATERMARK VIDEO ✅` }, { quoted: mek });
-  
-// SEND HD VIDEO
-await conn.sendMessage(from, { video: { url: tiktok.result.hdVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nNO-WATERMARK VIDEO ✅` }, { quoted: mek });
-  
-// SEND AUDIO
-await conn.sendMessage(from, { audio: { url: tiktok.result.sound }, mimetype: "audio/mpeg" }, { quoted: mek });
-
-  
 } catch (e) {
-console.log(e)
-reply(e)
+  reply(N_FOUND)
+  l(e)
 }
 })
 
-// https://chat.whatsapp.com/GzWbSKBkjcJARDc6wRvziv
+cmd({
+  pattern: "dau",
+  dontAddCommandList: true,
+  filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
+await conn.sendMessage(from, { document: { url: q }, mimetype: 'audio/mpeg', fileName: 'TikTok Audio' + '.mp3',caption: config.FOOTER }, { quoted: mek })
+await conn.sendMessage(from, { react: { text: '✔', key: mek.key }})
+} catch (e) {
+  reply('*ERROR !!*')
+l(e)
+}
+})
