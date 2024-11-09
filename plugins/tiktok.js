5244bf09e556e4452e1b7524c92861f45a70290c
Malaka-KG <darkalfhabot@gmail.com>
2024-11-09 17:34:14 +0530

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,111 +1,58 @@
-const config = require('../config')
-const { cmd, commands } = require('../command')
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
-const { Tiktok } = require('../lib/tiktok')
-function regtik(url) {
-return url.includes('tiktok.com')
-}
+// TIKTOK DOWNLOAD COMMAND
 
-var desc =''
-if(config.LANG === 'SI') desc = "Tiktok වෙතින් වීඩියෝ බාගත කරයි."
-else desc = "Download videos from Facebook."
+const { cmd } = require('../command')
+const { fetchJson } = require('../lib/functions')
 
-var N_FOUND =''
-if(config.LANG === 'SI') N_FOUND = "*මට කිසිවක් සොයාගත නොහැකි විය :(*"
-else N_FOUND = "*I couldn't find anything :(*"
-
-var urlneed =''
-if(config.LANG === 'SI') urlneed = "*කරුණාකර Tiktok video url එකක් ලබා දෙන්න*"
-else urlneed = "*Please give me tiktok video url..*"
 const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
+
 
 cmd({
     pattern: "tiktok",
-    alias: ["ttdl"],
-    react: '🏷️',
-    desc: desc,
+    alias: ["tt","ttdown"],
+    react: "🍧",
+    desc: "",
     category: "download",
-    use: '.tiktok <Tiktok link>',
+    use: '.tiktok < url >',
     filename: __filename
 },
-async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+async(conn, mek, m,{from, quoted, reply, q }) => {
 try{
-if (!regtik(q)) return await  reply(urlneed)
-var l = ''
+  
+if(!q) return await reply("Please give me tiktok url");
+  if(!q.includes('tiktok.com')) return await reply("This url is invalid");
+  
 const tiktok = await fetchJson(`${apilink}/download/tiktok?url=${q}`);
-if(tiktok.msg == 'OK') {
-let data = tiktok
-l = {
-title: data.result.desc,
-nowm: data.result.withoutWaterMarkVideo,
-watermark: data.result.waterMarkVideo,
-audio: data.result.music,
-thumbnail: data.result.cover,
-author: data.result.author
-}
-}
-else {
-let data = await Tiktok(q)
-l = data
-}
+  
+const msg = `
+           🍧 *TIKTOK DOWNLOADER* 🍧
 
 
-let dat = `[🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚]
+• *Title* - ${tiktok.result.title}
 
-*TIKTOK DOWNLOADER*
+• *Author* - ${tiktok.result.author}
 
-*📃 Title:* ${l.title}
-*✍🏼 Author:* ${l.author}`
+• *Duration* - ${tiktok.result.duration}
 
-let sections = [
-       {
-	title: "",
-	rows: [
-    {title: "1", rowId: `${prefix}dvideo ${l.nowm}`, description: 'video without Watermark'},
-    {title: "2", rowId: `${prefix}dvideo ${l.watermark}`, description: 'Video with Watermark'},
-    {title: "3", rowId: `${prefix}dau ${l.audio}`, description: 'Download audio'},
-	]
-    } 
-]
-const listMessage = {
-  image: { url: l.thumbnail },
-  caption: dat,
-  footer: config.FOOTER,
-  buttonText: "🔢 Reply below number,",
-  sections,
-contextInfo: {
-				
-				externalAdReply: { 
-					title: '🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚',
-					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
-					mediaType: 1,
-					sourceUrl: "" ,
-          thumbnailUrl: 'https://telegra.ph/file/ba8ea739e63bf28c30b37.jpg' ,
-					renderLargerThumbnail: false,
-          showAdAttribution: true
-         }}	
-}
+• *Views* - ${tiktok.result.views}   
+`
+  
+// SEND DETAILS
+await conn.sendMessage( from, { image: { url: tiktok.result.cover || '' }, caption: msg }, { quoted: mek });
 
-await conn.replyList(from, listMessage ,{ quoted : mek }) 
+// SEND WATER MARK VIDEO
+await conn.sendMessage(from, { video: { url: tiktok.result.wmVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nWATERMARK VIDEO ✅` }, { quoted: mek });
+  
+// SEND HD VIDEO
+await conn.sendMessage(from, { video: { url: tiktok.result.hdVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nNO-WATERMARK VIDEO ✅` }, { quoted: mek });
+  
+// SEND AUDIO
+await conn.sendMessage(from, { audio: { url: tiktok.result.sound }, mimetype: "audio/mpeg" }, { quoted: mek });
 
+  
 } catch (e) {
-  reply(N_FOUND)
-  l(e)
+console.log(e)
+reply(e)
 }
 })
 
-cmd({
-  pattern: "dau",
-  dontAddCommandList: true,
-  filename: __filename
-},
-async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
-await conn.sendMessage(from, { document: { url: q }, mimetype: 'audio/mpeg', fileName: 'TikTok Audio' + '.mp3',caption: config.FOOTER }, { quoted: mek })
-await conn.sendMessage(from, { react: { text: '✔', key: mek.key }})
-} catch (e) {
-  reply('*ERROR !!*')
-l(e)
-}
-})
+// https://whatsapp.com/channel/0029VaaPfFK7Noa8nI8zGg27
