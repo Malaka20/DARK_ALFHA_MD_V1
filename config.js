52317416f6d86d0c56a00088f58d642d5c66df0a
Malaka-KG <darkalfhabot@gmail.com>
2024-11-11 20:03:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "youre session id",
 MONGODB: process.env.MONGODB || "enter mongodb here",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/XWggNqz/20241027-181936.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/5Rp1c5q/20241111-195552.jpg",
 BOT_NUMBER: process.env.BOT_NUMBER || "94704243771",
 PREFIX: process.env.PREFIX || '.',
 LANG: process.env.BOT_LANG || 'EN' ,
