efde62e286daa24e73c9a7fda95ef865b3aca306
Malaka-KG <darkalfhabot@gmail.com>
2024-11-15 14:46:56 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -2,6 +2,7 @@
 
 const { cmd } = require('../command')
 const { fetchJson } = require('../lib/functions')
+const yts = require('yt-search')
 
 const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
 
@@ -47,3 +48,55 @@ console.log(e)
 reply(e)
 }
 })
+
+//video by 𝘔𝘈𝘓𝘈𝘒𝘈𝘉𝘖𝘛𝘔𝘋 🙋
+
+
+cmd({
+    pattern: "video",
+    desc: "downlode videos",
+    category: "downlode",
+    react: "🎬",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*Please give me a title*")
+let search = await yts(q)
+let link = search.all[0].url
+let desc = `
+*──────────────────*
+_*🌟 VIDEO DＯＷＮＬＯＤＥＲ 🌟*_
+*──────────────────*
+
+ *Title :* ${search.all[0].title}
+
+ *Description :* ${search.all[0].description}
+
+ *Duration :* ${search.all[0].timestamp}
+
+ *Ago :* ${search.all[0].ago}
+
+ *Views :* ${search.all[0].views}
+
+ *URL :* ${search.all[0].url}
+
+> 𝙳𝙰𝚁𝙺-𝙰𝙻𝙵𝙷𝙰-𝙱𝙾𝚃 👩‍💻
+`
+
+await conn.sendMessage(from,{image:{url: search.all[0].thumbnail},caption:desc},{quoted:mek})
+
+
+        let data = await fetchJson (`https://api.giftedtech.my.id/api/download/ytvideo?apikey=gifted&url=${link}`)
+
+await conn.sendMessage(from, {
+  video: {url: data.result.downloadLink},
+mimetype: "video/mp4",
+ fileName: `${data.result.title}.mp4`,caption: `*© 𝘔𝘢𝘭𝘢𝘬𝘢 · · ·* 👩‍💻`}, { quoted: mek })
+
+}catch(e){
+    console.log(e)
+    reply(`${e}`)
+}
+}
+)
