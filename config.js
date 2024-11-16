c0ff5b80f3a4fb4a162a3bf24411fad2a96bf17c
Malaka-KG <darkalfhabot@gmail.com>
2024-11-17 01:59:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,6 @@ SESSION_ID: process.env.SESSION_ID || "youre session id",
 MONGODB: process.env.MONGODB || "enter mongodb here",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/5Rp1c5q/20241111-195552.jpg",
 BOT_NUMBER: process.env.BOT_NUMBER || "94704243771",
-PREFIX: process.env.PREFIX || '.',
 LANG: process.env.BOT_LANG || 'EN' ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 };
