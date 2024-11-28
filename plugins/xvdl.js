696c397e6601e681dc4c9e82fe2af2cdc68aa30e
Malaka-KG <darkalfhabot@gmail.com>
2024-11-29 01:11:41 +0530

    Delete plugins/xvdl.js

--- plugins/xvdl.js
+++
@@ -1,9 +0,0 @@
-// XVIDEO DOWNLOAD COMMAND
-
-const { cmd } = require('../command')
-const { fetchJson } = require('../lib/functions')
-
-const apilink = 'https://www.dark-yasiya-api.site' // API LINK ( DO NOT CHANGE THIS!! )
-
-
-
