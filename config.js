167caf4bb1b684ccbd591cbd1a09ed09e8a044f3
Malaka-KG <darkalfhabot@gmail.com>
2024-11-30 17:18:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,4 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/5Rp1c5q/20241111-195552.jp
 BOT_NUMBER: process.env.BOT_NUMBER || "94704243771",
 LANG: process.env.BOT_LANG || 'EN' ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
-AUTO_REACT: process.env.AUTO_REACT || "true",
-HEART_REACT: process.env.HEART_REACT || "true",
-OWNER_REACT: process.env.OWNER_REACT || "true",
 };
