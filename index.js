f2960b4fe692eb85e2a3e6e5bcbfc10a22d41a59
Malaka-KG <darkalfhabot@gmail.com>
2024-12-01 23:05:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `DARK-ALFHA-MD-BOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://i.ibb.co/V2pdBTJ/20241111-195632.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://i.ibb.co/QNwLWTN/20241201-230018.jpg` }, caption: up })
 
 }
 })
