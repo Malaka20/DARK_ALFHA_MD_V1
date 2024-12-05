c065cc1a81b051fb105e9cf99d6d86e256eed804
Malaka-KG <darkalfhabot@gmail.com>
2024-12-06 00:05:17 +0530

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -755,3 +755,157 @@ reply(e)
 )
 
 // FOLLOW US:  𝘔𝘈𝘓𝘈𝘒𝘈
+
+cmd({
+    pattern: "mfire",
+    alias: ["mf","mediafire"],
+    react: "🔥",
+    desc: "",
+    category: "download",
+    use: '.mfire < mediafire url >',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, reply, q }) => {
+try{
+  
+if(!q) return await reply("Please give me mediafire url");
+  if(!q.includes('mediafire.com')) return await reply("This url is invalid");
+  
+const mfire = await fetchJson(`${apilink}/download/mfire?url=${q}`);
+  
+const msg = `
+╭─────────────────❖
+│🔥*MEDIAFIRE DOWNLOADER*🔥
+╰─────────────────❖
+──────────────────❖
+╭────────────────❖
+│ ℹ️ *DARK_ALFHA_MD* 
+│
+│☍ ⦁ *File Name* - ${mfire.result.fileName}
+│☍ ⦁ *File Size* - ${mfire.result.size}
+│☍ ⦁ *Upload Date and Time* - ${mfire.result.date}
+╰────────────────❖
+──────────────────❖
+> ᴍᴀʟᴀᴋᴀ-ᴍᴅ ʙʏ ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ . . . 👩‍💻
+`
+  
+// SEND DETAILS
+await conn.sendMessage( from, { image: { url: 'https://i.ibb.co/dPw1fHD/mfire.jpg' }, caption: msg }, { quoted: mek });
+
+// SEND FILE
+await conn.sendMessage(from, { document: { url: mfire.result.dl_link }, mimetype: mfire.result.fileType , fileName: mfire.result.fileName, caption: mfire.result.fileName }, { quoted: mek });
+
+  
+} catch (e) {
+console.log(e)
+reply('This url type is not working !!')
+}
+})
+
+cmd({
+    pattern: "tiktok2",
+    alias: ["tt2","ttdown2"],
+    react: "📥",
+    desc: "",
+    category: "download",
+    use: '.tiktok < url >',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, reply, q }) => {
+try{
+  
+if(!q) return await reply("Please give me tiktok url");
+  if(!q.includes('tiktok.com')) return await reply("This url is invalid");
+  
+const tiktok = await fetchJson(`${apilink}/download/tiktok?url=${q}`);
+  
+const msg = `
+╭─────────────────❖
+│📥*TIKTOK DOWNLOADER*📥
+╰─────────────────❖
+──────────────────❖
+╭────────────────❖
+│ ℹ️ *DARK_ALFHA_MD* 
+│
+│☍ ⦁ *Title* - ${tiktok.result.title}
+│☍ ⦁ *Author* - ${tiktok.result.author}
+│☍ ⦁ *Duration* - ${tiktok.result.duration}
+│☍ ⦁ *Views* - ${tiktok.result.views}
+╰────────────────❖
+──────────────────❖
+> ᴍᴀʟᴀᴋᴀ-ᴍᴅ ʙʏ ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ . . . 👩‍💻
+`
+  
+// SEND DETAILS
+await conn.sendMessage( from, { image: { url: tiktok.result.cover || '' }, caption: msg }, { quoted: mek });
+
+// SEND WATER MARK VIDEO
+await conn.sendMessage(from, { video: { url: tiktok.result.wmVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}WATERMARK VIDEO ✅` }, { quoted: mek });
+  
+// SEND HD VIDEO
+await conn.sendMessage(from, { video: { url: tiktok.result.hdVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}NO-WATERMARK VIDEO ✅` }, { quoted: mek });
+  
+// SEND AUDIO
+await conn.sendMessage(from, { audio: { url: tiktok.result.sound }, mimetype: "audio/mpeg" }, { quoted: mek });
+
+  
+} catch (e) {
+console.log(e)
+reply(e)
+}
+})
+
+cmd({
+    pattern: "xvideo",
+    alias: ["xvdl","xvdown"],
+    react: "🔞",
+    desc: "Download xvideo.com porn video",
+    category: "download",
+    use: '.xvideo < text >',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, reply, q }) => {
+try{
+
+  if(!q) return await reply("Please give me few word !")
+    
+const xv_list = await fetchJson(`${apilink}/search/xvideo?q=${q}`)
+if(xv_list.result.length < 0) return await reply("Not results found !")
+
+const xv_info = await fetchJson(`${apilink}/download/xvideo?url=${xv_list.result[0].url}`)
+    
+  // FIRST VIDEO
+  
+const msg = `
+╭─────────────────❖
+│🔞*XVIDEO DOWNLOADER*🔞
+╰─────────────────❖
+ ──────────────────❖
+╭────────────────❖
+│ ℹ️ *DARK_ALFHA_MD* 
+│
+│☍ ⦁ *Title* - ${xv_info.result.title}
+│☍ ⦁ *Views* - ${xv_info.result.views}
+│☍ ⦁ *Like* - ${xv_info.result.like}
+│☍ ⦁ *Deslike* - ${xv_info.result.deslike}
+│☍ ⦁ *Size* - ${xv_info.result.size}
+╰────────────────❖
+──────────────────❖
+> ᴍᴀʟᴀᴋᴀ-ᴍᴅ ʙʏ ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ . . . 👩‍💻
+`
+
+
+await conn.sendMessage( from, { image: { url: xv_info.result.image || '' }, caption: msg }, { quoted: mek })
+
+// XVIDEO
+await conn.sendMessage(from, { video: { url: xv_info.result.dl_link }, mimetype: "video/mp4", fileName: xv_info.result.title, caption: xv_info.result.title }, { quoted: mek });
+
+// SEND VIDEO
+await conn.sendMessage(from, { document: { url: xv_info.result.dl_link }, mimetype: "video/mp4", fileName: xv_info.result.title, caption: xv_info.result.title }, { quoted: mek });
+
+
+} catch (error) {
+console.log(error)
+reply(error)
+}
+})
