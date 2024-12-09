a1fa6731e161abb6e7905cba626245ba192b7a59
Malaka20/NETHU-MD-V1 2 <153556272+Malaka20@users.noreply.github.com>
2024-12-10 02:23:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "youre session id",
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "wEIxUDBL#3W7t4Cw5TKba1pHP42RJJHzhpnp2V-HLy2C60J6Y3D0",
+MONGODB: process.env.MONGODB || "mongodb://mongo:PIDRqpahCzzZlpwYFWqShxAURtcFoRje@autorack.proxy.rlwy.net:32217",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/QNwLWTN/20241201-230018.jpg",
 BOT_NAME: process.env.BOT_NAME || "➺ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ࿐",
 LANG: process.env.BOT_LANG || 'EN' ,
